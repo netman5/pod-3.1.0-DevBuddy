10ea210e2d48c5d990c9a578fd497b912dd4f7bd
Ola Ishola <45001916+netman5@users.noreply.github.com>
2021-08-01 19:01:38 +0100

    changed the popup message

--- code/src/extension.ts
+++ code/src/extension.ts
@@ -13,13 +13,14 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('code.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from code!');
-	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+    vscode.commands.registerCommand("code.helloWorld", () => {
+      vscode.window.showInformationMessage(
+        "Thank You for choosing to use code!"
+      );
+    })
+  );
 }
 
 // this method is called when your extension is deactivated
