ab6b34b5d3b6e65e1440e56ccdc1017864f0c4f7
tomarviii88 <tomarviii88@gmail.com>
2021-08-02 16:58:08 +0530

    project app scaffolding

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Dev Buddy</h1>
+        <p> A bookmark management chrome extension</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
