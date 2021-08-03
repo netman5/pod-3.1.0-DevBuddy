de5aea6c5f1a643cfd8c121b3f48b54665030c12
Ola Ishola <45001916+netman5@users.noreply.github.com>
2021-08-03 16:53:09 +0100

    added modal component and style

--- app/src/components/Category.js
+++ app/src/components/Category.js
@@ -2,8 +2,10 @@ import React from 'react';
 import mockData from '../utils/mock';
 import styles from './style/category.module.css';
 import AddIcon from '@material-ui/icons/Add';
+import TabModal from './TabModal'
 
 function Category() {
+
   return (
     <div className={styles.container}>
       <div className={styles.head}>
@@ -14,13 +16,14 @@ function Category() {
       </div>
       {mockData.map((data) => {
         return (
-          <div className={styles.cardContainer}>
+          <div className={styles.cardContainer} >
             <div className={styles.categoryName}>{data.name}</div>
             <div className={styles.groups}>
               {data.groups.map((group) => {
                 const {name, tabs} = group
                 
                 return (
+                  
                   <div className={styles.card}>
                     <h3 className={styles.text}> {name}</h3> 
                     <span className={styles.text}>{tabs.length} Tabs </span>
