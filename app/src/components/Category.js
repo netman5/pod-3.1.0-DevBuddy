ddf6cf06d7d7a36000fc9f735c141b0c50526d68
Tushar3099 <tusharneogi30@gmail.com>
2021-08-05 19:24:34 +0530

    populated modal tab with the tabs information and improved the UI

--- app/src/components/Category.js
+++ app/src/components/Category.js
@@ -2,10 +2,16 @@ import React from 'react';
 import mockData from '../utils/mock';
 import styles from './style/category.module.css';
 import AddIcon from '@material-ui/icons/Add';
-import TabModal from './TabModal'
+import TabModal from './TabModal';
 
 function Category() {
   const [open, setOpen] = React.useState(false);
+  const [currTabData, setCurrentTabData] = React.useState(null);
+
+  const handleTabClick = (group) => {
+    setOpen(true);
+    setCurrentTabData(group);
+  };
 
   return (
     <div className={styles.container}>
@@ -21,12 +27,15 @@ function Category() {
             <div className={styles.categoryName}>{data.name}</div>
             <div className={styles.groups}>
               {data.groups.map((group) => {
-                const {name, tabs} = group
-                
+                const { name, tabs } = group;
+
                 return (
-                  
-                  <div className={styles.card} key={name} onClick={()=> setOpen(true)}>
-                    <h3 className={styles.text}> {name}</h3> 
+                  <div
+                    className={styles.card}
+                    key={name}
+                    onClick={() => handleTabClick(group)}
+                  >
+                    <h3 className={styles.text}> {name}</h3>
                     <span className={styles.text}>{tabs.length} Tabs </span>
                   </div>
                 );
@@ -36,8 +45,7 @@ function Category() {
         );
       })}
 
-    {open && < TabModal closeModal={setOpen}  open={open}/>}
-
+      {open && <TabModal closeModal={setOpen} data={currTabData} open={open} />}
     </div>
   );
 }
--- app/src/components/TabModal.js
+++ app/src/components/TabModal.js
@@ -1,33 +1,49 @@
-import React from 'react'
-import styles from '../components/style/modal.module.css'
-
+import React from 'react';
+import styles from '../components/style/modal.module.css';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 
 function TabModal(props) {
-  const {closeModal} = props
-  
-
+  const { closeModal, data } = props;
+  console.log(data);
   return (
     <div className={styles.modalBackground}>
       <div className={styles.modalContainer}>
-        <div className= {styles.titleCloseBtn}>
-        <button onClick={() => closeModal(false)}> X </button>
+        <div className={styles.titleCloseBtn}>
+          <button onClick={() => closeModal(false)}> X </button>
         </div>
-        <div className={styles.title}>
-          <h1>Title</h1>
-        </div>
+        <h1 className={styles.title}>{data.name}</h1>
 
-        <div className={styles.body}> 
-          <p>This will contain the Titles and url of tabs</p>
+        <div className={styles.body}>
+          <div className={styles.tabs}>
+            {data.tabs.map((item) => {
+              return (
+                <div className={styles.tab}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        // checked={state.checkedB}
+                        // onChange={handleChange}
+                        name='checkedB'
+                        color='primary'
+                      />
+                    }
+                    className={styles.tab}
+                  />
+                  <span className={styles.tabName}>{item.title}</span>
+                </div>
+              );
+            })}
+          </div>
         </div>
         <div className={styles.modalFooter}>
-          <button id={styles.closeBtn} onClick={() => closeModal(false)}>Close</button>
+          <button id={styles.closeBtn} onClick={() => closeModal(false)}>
+            Close
+          </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default TabModal
-
-
-
+export default TabModal;
