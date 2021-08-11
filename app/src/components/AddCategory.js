c2f82790e5be18bf6f8113739503cdb19dd82991
Ola Ishola <45001916+netman5@users.noreply.github.com>
2021-08-11 23:07:39 +0100

    ui refactor and btn capture current window

--- app/src/components/AddCategory.js
+++ app/src/components/AddCategory.js
@@ -3,8 +3,7 @@ import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import styles from "../components/style/modal.module.css";
-import MockData from "../utils/mock";
-import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import { Typography } from "@material-ui/core";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -21,11 +20,13 @@ const useStyles = makeStyles((theme) => ({
       marginTop: '25px'
     },
   },
+
+  text: {
+    color: 'black'
+  }
 }));
 
 function AddCategory({ ModalState, setGlobal}) {
-  const [categoryFound, setCategoryFound] = useState(false)
-  const [groupFound, setGroupFound] = useState(false)
   const [category, setCategory] = useState('');
   const [group, setGroup] = useState('');
   const [tabs, setTabs] = useState([])
@@ -65,7 +66,6 @@ function AddCategory({ ModalState, setGlobal}) {
               if (storeData[i].groups[j].name === group) {
                 group_found = true
                 console.log('group found');
-                // storeData[i].groups[j].tabs = [...storeData[i].groups[j].tabs, tabs];
                  for (let k = 0; k < tabs.length; k++) {
                   storeData[i].groups[j].tabs.push(tabs[k]);
                 }
@@ -101,18 +101,19 @@ function AddCategory({ ModalState, setGlobal}) {
     event.preventDefault();
   };
 
-
-  const handleClose = () => {
-    ModalState(false)
-  }
-
   const classes = useStyles();
 
   return (
     <div className={styles.modalBackground}>
       <div className={styles.addModalContainer}>
         <div className={styles.body}>
-          <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit} onClick={getTabs}>
+
+          <form className={classes.root} 
+            noValidate 
+            autoComplete="off" 
+            onSubmit={handleSubmit} 
+            onClick={getTabs}>
+
             <TextField
               value={category}
               id="outlined-primary"
@@ -134,31 +135,12 @@ function AddCategory({ ModalState, setGlobal}) {
               size='small'
               onChange={(e) => setGroup(e.target.value)}
             />
-
-            {/* <TextField
-              value={tabs}
-              id="outlined-secondary"
-              label="Add group name"
-              variant="outlined"
-              color="secondary"
-              required={true}
-              size='small'
-              onChange={(e) => setTabs(e.target.value)}
-            /> */}
-
-            {/* <TextareaAutosize
-              value={tabs}
-              name='tabs'
-              className={styles.textArea}
-              aria-label="maximum height"
-              placeholder='Enter your url lists'
-              onChange={(e) => setTabs(e.target.value)}
-            /> */}
-
+            <Typography className={classes.text} variant='body1'>Click add to capture current window tabs.</Typography>
+            
             <div className={styles.modalFooter}>
               <button 
-              id={styles.closeBtn}
-              onClick={handleClose}
+              className={styles.closeAddModalBtn}
+              onClick={() => ModalState(false)}
               >
                 Close
               </button>
